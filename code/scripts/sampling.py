a95390480fc0b3739400d827bac770817f780e01
Ruochen Miao <boomber_man_@outlook.com>
2024-04-26 12:02:42 -0400

    fix import bug

--- code/scripts/sampling.py
+++ code/scripts/sampling.py
@@ -2,11 +2,12 @@ import os
 import random
 import shutil
 
-from ..local_config import ROOT_PATH, DESTINATION_PATH
-NUMBER_OF_PHOTOS = 10
+from local_config import ROOT_PATH, DESTINATION_PATH
+NUMBER_OF_PHOTOS = 1000
 SEED = "CloudComputing2024Spring"
 
 if __name__ == "__main__":
+    print(os.getcwd())
     random.seed(SEED)
     image_folder_paths = []
     for split in ["train", "valid", "test"]:
@@ -17,6 +18,5 @@ if __name__ == "__main__":
             for image in images:
                 image_paths.append(os.path.join(image_folder_path, image))
     sampled = random.sample(image_paths, k=NUMBER_OF_PHOTOS)
-    print(sampled)
     for id, sample in enumerate(sampled):
         shutil.copyfile(src=sample, dst=os.path.join(DESTINATION_PATH, str(id) + ".png"))

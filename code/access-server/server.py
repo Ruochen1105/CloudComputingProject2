d5daadc0d01c8ebf2d6430727273921747879192
Ruochen Miao <boomber_man_@outlook.com>
2024-04-26 23:44:52 -0400

    add comments

--- code/access-server/server.py
+++ code/access-server/server.py
@@ -4,40 +4,46 @@ _PORT = 8421
 _MASTER = [None, None]  # (host: str, port: str)
 
 class _MyHTTPServer(BaseHTTPRequestHandler):
+    # answering HTTP GET
     def do_GET(self):
+
         global _MASTER
+
         if self.path == '/':
             self.send_response(200)
             self.send_header('Content-type', 'text/plain')
-            if _MASTER == [None, None]:
+            if _MASTER == [None, None]:    # when there is no master node
                 _MASTER[0] = self.client_address[0]
                 _MASTER[1] = self.headers["port"]
                 self.send_header("Master", "True")
                 self.end_headers()
                 self.wfile.write(b"You are the master node.")
-            else:
+            else:   # when there is master node
                 self.send_header("Master_host", _MASTER[0])
                 self.send_header("Master_port", _MASTER[1])
                 self.send_header("Master", "False")
                 self.end_headers()
                 self.wfile.write(b"Please find the host and port of the master in the header.")
+
         elif self.path == '/health':
             self.send_response(200)
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
             self.wfile.write(b"The access server is working properly.")
+
         elif self.path == "/leave":
-            if _MASTER[0] == self.client_address[0]:
+            if _MASTER[0] == self.client_address[0]:    # if is from the master
                 self.send_response(200)
                 self.send_header('Content-type', 'text/plain')
                 self.end_headers()
                 _MASTER = [None, None]
                 self.wfile.write(b"Left.")
-            else:
+            else:    # if is not from the master
                 self.send_response(403)
                 self.send_header('Content-type', 'text/plain')
                 self.end_headers()
                 self.wfile.write(b"Forbidden.")
+
         else:
             self.send_response(404)
             self.send_header('Content-type', 'text/plain')

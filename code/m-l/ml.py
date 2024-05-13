41bc8f9a7b4f4e59806323a728e14b0629a1ac56
Ruochen Miao <74287387+Ruochen1105@users.noreply.github.com>
2024-05-13 09:38:16 +0800

    fix git name

--- code/m-l/ml.py
+++ code/m-l/ml.py
@@ -10,12 +10,11 @@ from google.cloud.aiplatform import Endpoint
 import local_config
 
 
-# endpoint = Endpoint(endpoint_name=local_config.endpoint_id, project=local_config.project, location=local_config.location, credentials=)
-
+endpoint = Endpoint(endpoint_name=local_config.endpoint_id, project=local_config.project, location=local_config.location, credentials=)
 
 
 if __name__ == "__main__":
     im = Image.open("./assets/accidents/0.png")
     im.show()
-    # endpoint.predict(instances=[])
+    endpoint.predict(instances=[])
 
